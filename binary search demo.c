060a9a72180c49d7360ebbddc68f08645e9fc630
subash868subash-del <subash868subash@gmail.com>
2026-02-06 14:13:47 +0530

    add file

---
+++ binary search demo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int binarySearch(int arr[], int n, int key){
+    int low = 0,high = n-1;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(arr[mid] == key)
+           return mid;
+        else if(arr[mid] < key)
+           low = mid +1;
+        else
+           high = mid - 1;
+    }
+}
+int main() {
+    int arr[] ={10,20,30,40,50,60};
+    int n = 6, key = 40;
+    int result = binarySearch(arr, n, key);
+    if(result != -1)
+      printf("Element found at index",result);
+    else
+      printf("Element not found");
+      
+      return 0;
+}
